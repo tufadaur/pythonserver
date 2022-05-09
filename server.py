6bac9f62b9729782700dd1f18b49f6c860b82347
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-09 11:45:57 +0200

    Update server.py

--- server.py
+++ server.py
@@ -16,9 +16,29 @@ def search():
    employee_data= json.load(f)
    
    temp1 = request.args.get('temp1')
+   temp2 = request.args.get('temp2')
+   temp3 = request.args.get('temp3')
+   stato1 = request.args.get('stato1')
+   stato2 = request.args.get('stato2')
+   stato3 = request.args.get('stato3')
   
   if(temp1 is not None):
     employee_data["temp1"] = temp1
+
+  if(temp2 is not None):
+    employee_data["temp2"] = temp2
+     
+  if(temp3 is not None):
+    employee_data["temp3"] = temp3
+
+  if(stato1 is not None):
+    employee_data["stato1"] = stato1
+    
+  if(stato2 is not None):
+    employee_data["stato2"] = stato2
+    
+  if(stato3 is not None):
+    employee_data["stato3"] = stato3
   
   
   
