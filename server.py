26cb590c29db87001d11ff58e97fbdebc653f27b
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-09 10:40:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask , request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "Hello world!" 
+  
+  
+@app.route('/termostato/')
+def termostato():
+    temp1 = request.args.get('temp1')
+    return temp1
 
 if __name__ == '__main__':
     app.debug = True
