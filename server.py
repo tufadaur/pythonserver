346147591cfea0669d473daf6fca82db409d09a8
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-10 11:25:45 +0200

    aa

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask , request
+from flask import Flask, render_template , request
 from datetime import datetime
 import json
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Hello world!" 
+  return render_template('index.html') 
   
   
 @app.route('/termostato' , methods=['GET'])
