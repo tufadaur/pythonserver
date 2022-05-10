7f4f4608e13d112903f90618761764febb0f7ef3
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-10 12:36:27 +0200

    a

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function aggiorna_temperatura() {
+    document.getElementById("tempinterna").innerText = "{{database.tinterna}}";
+}
+
+setInterval(aggiorna_temperatura, 1000);
--- templates/script.js
+++
@@ -1,3 +0,0 @@
-<script>
-    document.getElementById("tinterna").innerHTML = "{{database.tinterna}}";
-</script>
