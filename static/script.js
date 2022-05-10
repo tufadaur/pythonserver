4d29e4d6d4a8418fd5d46cf84b4f7633002a338a
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-10 12:42:04 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 function aggiorna_temperatura() {
-    document.getElementById("tempinterna").innerText = "{{database.tinterna}}";
+    document.getElementById("tempinterna").innerText = {{database.tinterna}} ;
 }
 
 setInterval(aggiorna_temperatura, 1000);
