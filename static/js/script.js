7e67aea99650d3aef2c02cfe02fc24206c9d23a2
tufadaur <82369020+tufadaur@users.noreply.github.com>
2022-05-12 13:04:56 +0200

    a

--- static/js/script.js
+++ static/js/script.js
@@ -6,16 +6,18 @@ function aggiornadati(){
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
             document.getElementById("t_interna").innerHTML = myArr['t_interna'];
+            document.getElementById("caldaia").innerHTML = myArr['stato_caldaia'];
+            document.getElementById("t_esterna").innerHTML = myArr['t_esterna'];
+            document.getElementById("h_interna").innerHTML = myArr['h_interna'];
+            document.getElementById("h_esterna").innerHTML = myArr['h_esterna'];
+            document.getElementById("soglia").innerHTML = myArr['soglia_attuale'];
+            document.getElementById("confort").innerHTML = myArr['temperature'][0];
+            document.getElementById("eco").innerHTML = myArr['temperature'][1];
+            document.getElementById("hot").innerHTML = myArr['temperature'][2];
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-    
-    function myFunction(arr) {
-        document.getElementById("t_interna").innerHTML = arr['t_interna'];
-    }
-
-    
 }
 
 setInterval (aggiornadati , 1000) ;
